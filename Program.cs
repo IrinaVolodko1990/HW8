6247c3fd30177f6dbbf3513accdbb2e7bf6d8cf8
IrinaVolodko <8232734@gmail.com>
2023-07-31 20:09:44 +0300

    Second task

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+
 int[,] CreateDoubleArray(int row, int column)
 {
     int[,] doubleArr = new int[row, column];
@@ -99,3 +101,33 @@ void PrintDoubleArray(int[,] doubleArr)
         Console.WriteLine();
     }
 }
+int[] FindSumElementInRow(int[,] doubleArr)
+{
+    int row = doubleArr.GetLength(0);
+    int column = doubleArr.GetLength(1);
+    int[] sumArray = new int[row];
+    int rowSum = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            rowSum += doubleArr[i, j];
+        }
+        sumArray[i] = rowSum;
+        rowSum = 0;
+    }
+    return sumArray;
+}
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[,] doubleArray = CreateDoubleArray(3,2);
+PrintDoubleArray(doubleArray);
+int[] sumArray = FindSumElementInRow(doubleArray);
+PrintArray(sumArray);
